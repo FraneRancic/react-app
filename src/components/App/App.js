ccbb3a8386e47f70ae108eb9703fe0a36b26d5c6
FraneRancic <frane.rancic123@gmail.com>
2019-05-31 10:44:46 +0200

    inicial project structure

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      Header
+      <div className="container--flex wrapper">Sidebar + Main</div>
+    </div>
+  );
+}
+
+export default App;
